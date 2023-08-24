aa9a5975901b015bf19e89e0e706e53e63a9a26c
Dev Codru <harshitshivam001@gmail.com>
2023-08-24 20:35:02 +0530

    todo app

--- react/state/src/App.jsx
+++ react/state/src/App.jsx
@@ -1,25 +1,58 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
-  const [todoList, setTodoList] = useState([]);
-  const addTodoList = () => {
-    setTodoList((prevVal) => {
-      return [...prevVal, ]
-    })
-  }
-  return (
-    <>
-      <h1>To Do List</h1>
-      <input type="text" name="name" />
-      <button>Add Todo</button>
-      <ol>{
-        todoList.map((val, idx) => {
-          return <li key={idx}>{val}</li>
-        })
-      }</ol>
 
-    </>
+  const [list, setList] = useState([]);
+  const [task, setTask] = useState("");
+  const submitTask = (e) => {
+    e.preventDefault();
+    setList((prevVal) => {
+      return [...prevVal, task];
+    })
+    setTask("");
+  }
+
+  const deleteListItem = (idx) => {
+    setList(list.filter((val, index) => index != idx));
+  }
+
+  return (
+    <div className='app'>
+      <h1>To Do List</h1>
+      <Form task={task} setTask={setTask} submitTask={submitTask} />
+      <TaskList list={list} deleteListItem={deleteListItem} />
+    </div>
+  )
+}
+
+function TaskList({ list, deleteListItem }) {
+  return (
+    <div className='list'>
+      {
+        list != null &&
+        list.map((val, idx) => {
+          return <div className="list-item" key={idx}>{val}
+            <span className='delete'>✓</span>
+            <span onClick={() => deleteListItem(idx)} className='delete'>X</span>
+          </div>
+        })
+      }
+    </div>
+  )
+}
+
+function Form({ task, setTask, submitTask }) {
+  return (
+    <form onSubmit={(e) => submitTask(e)}>
+      <input type="text"
+        onChange={(e) => setTask(e.target.value)}
+        value={task}
+        name="task"
+        placeholder='Enter your task here...'
+      />
+      <input type="submit" value="Submit" />
+    </form>
   )
 }
 
