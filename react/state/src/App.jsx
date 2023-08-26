bcdaf5ac28406648768b2a5ffaffa430f1059bbb
Dev Codru <harshitshivam001@gmail.com>
2023-08-26 12:46:41 +0530

    conditional rendering

--- react/state/src/App.jsx
+++ react/state/src/App.jsx
@@ -5,6 +5,13 @@ function App() {
 
   const [list, setList] = useState([]);
   const [task, setTask] = useState("");
+  const [background, setBackground] = useState(true);
+  const [showList, setShowList] = useState(true);
+
+  let backgroundStyle = background === true ?
+    { backgroundColor: "#fff", color: "#000" } :
+    { backgroundColor: "#000", color: "#fff" };
+
   const submitTask = (e) => {
     e.preventDefault();
     setList((prevVal) => {
@@ -18,10 +25,15 @@ function App() {
   }
 
   return (
-    <div className='app'>
-      <h1>To Do List</h1>
-      <Form task={task} setTask={setTask} submitTask={submitTask} />
-      <TaskList list={list} deleteListItem={deleteListItem} />
+    <div className='app' style={backgroundStyle}>
+      <button onClick={() => setBackground(!background)}>Change Theme</button>
+      <button onClick={() => setShowList(!showList)}>Show Todo List</button>
+      {showList && (
+        <>
+          <h1>To Do List</h1>
+          <Form task={task} setTask={setTask} submitTask={submitTask} />
+          <TaskList list={list} deleteListItem={deleteListItem} />
+        </>)}
     </div>
   )
 }
